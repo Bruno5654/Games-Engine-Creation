71713cadad2428f59477e4939644c6b6aeeb504c
Bruno5654 <114944836+Bruno5654@users.noreply.github.com>
2022-11-11 10:28:13 +0000

    Finishing week 5

---
+++ Week6/Program25Initials/Program25Initials/Program25Initials.cpp
@@ -0,0 +1,19 @@
+// Program25Initials.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	char userName[50];
+	string userInital1, userInital2, userLastName;
+
+	cout << "Please enter your first and second initals followed by your full lastname seperated by spaces.\n";
+	cin.getline(userName, 50);
+
+	for (int i = 0; i < (userName))
+
+	return 0;
+}
