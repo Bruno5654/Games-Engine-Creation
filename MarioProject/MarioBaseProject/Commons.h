c67dd99e5080f71272b4750bdc62e1ec6cd9194a
Bruno Reeve <114944836+Bruno5654@users.noreply.github.com>
2023-03-03 10:25:48 +0000

    Week3 Beginings

---
+++ MarioProject/MarioBaseProject/Commons.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Vector2D
+{
+	float x;
+	float y;
+
+	Vector2D()
+	{
+		x = 0.0f;
+		y = 0.0f;
+	}
+
+	Vector2D(float inital_x, float inital_y)
+	{
+		x = inital_x;
+		y = inital_y;
+	}
+};
